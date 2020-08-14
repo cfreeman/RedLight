09f22380b540c9c1a37e67c45033636c33941e37
clinton <clinton.freeman@gmail.com>
2020-08-14 12:55:23 +1000

    first build.

---
+++ precache-manifest.78cd19a8edad2afb9c11c5195af5ca9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07506344e1501a04a8ff5f5c3bab1ce0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecbe49211e0715f8d280",
+    "url": "/static/js/2.9213a71a.chunk.js"
+  },
+  {
+    "revision": "dde2f98dd0ed8bfad18d88defdcf5f43",
+    "url": "/static/js/2.9213a71a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66ad3c3d89b748883fcc",
+    "url": "/static/js/main.dc5e1735.chunk.js"
+  },
+  {
+    "revision": "04b4970dbbc02ecb46dd",
+    "url": "/static/js/runtime-main.f483ed76.js"
+  }
+]);
