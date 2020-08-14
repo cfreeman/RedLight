c893ca3539fa9b078e8c3c3feae7e9364f27ac10
clinton <clinton.freeman@gmail.com>
2020-08-14 13:05:48 +1000

    Set URL.

---
+++ precache-manifest.d36b8bcedb3bacee1fccdeecd5f1fec8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff01866a28d2ad978664752842b0011",
+    "url": "/RedLight/index.html"
+  },
+  {
+    "revision": "ecbe49211e0715f8d280",
+    "url": "/RedLight/static/js/2.9213a71a.chunk.js"
+  },
+  {
+    "revision": "dde2f98dd0ed8bfad18d88defdcf5f43",
+    "url": "/RedLight/static/js/2.9213a71a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66ad3c3d89b748883fcc",
+    "url": "/RedLight/static/js/main.dc5e1735.chunk.js"
+  },
+  {
+    "revision": "64c6feff1579ed920be0",
+    "url": "/RedLight/static/js/runtime-main.445e1e44.js"
+  }
+]);
